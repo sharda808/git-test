7eeeb8dc9effcc1032df579efda833eeadbcb6a5
sharda <shardakumari96115@gmail.com>
2026-01-06 16:02:51 +0530

    Adding paragraph

--- e-commerce-dashboard/src/App.jsx
+++ e-commerce-dashboard/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
    <h1>E-commerce-dashboard</h1>
    <Sidebar />
 <h2>HI sharda</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, doloremque?</p>
     </>
   )
 }
